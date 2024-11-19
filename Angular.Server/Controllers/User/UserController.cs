93ad903a706f94b97e1afa26a5384a6285d19f51
105615691 <105615691@omidtech-co.local>
2024-11-19 10:13:10 +0330

    push 1

---
+++ Angular.Server/Controllers/User/UserController.cs
@@ -0,0 +1,15 @@
+﻿namespace Angular.Server.Controllers.User;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult GetAll()
+    {
+        return Ok(new List<UserDto>
+        {
+            new UserDto { Id = 1, UserName = "komaei@live.com" },
+            new UserDto { Id = 1, UserName = "mammadkoma@gmail.com" }
+        });
+    }
+}
---
+++ Angular.Server/Controllers/User/UserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Angular.Server.Controllers.User;
+
+public class UserDto
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
