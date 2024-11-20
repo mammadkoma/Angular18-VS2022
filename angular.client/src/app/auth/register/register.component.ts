25aba63255316e55b6a7c082a75cfaac41cbf2a6
105615691 <105615691@omidtech-co.local>
2024-11-20 13:34:48 +0330

    mat-toolbar

--- angular.client/src/app/auth/register/register.component.ts
+++ angular.client/src/app/auth/register/register.component.ts
@@ -2,11 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   constructor(private http: HttpClient) { }
--- angular.client/src/app/components/app.component.ts
+++ angular.client/src/app/components/app.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatDividerModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  standalone: true,
-  imports: [RouterOutlet],
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor() { }
-
-  ngOnInit() {
-    
-  }
-
-  title = 'angular.client';
+  title = 'ng';
 }
+
+//@Component({
+//    selector: 'app-root',
+//    templateUrl: './app.component.html',
+//    styleUrl: './app.component.css',
+//})
+//export class AppComponent {
+//  constructor() { }
+
+//  ngOnInit() {
+    
+//  }
+
+//  title = 'angular.client';
+//}
